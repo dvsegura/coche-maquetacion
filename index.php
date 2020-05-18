bd3cd822a2e51270a2f4c94162dc5d32c961f8e1
Daylin <daylinfeina@gmail.com>
2020-05-18 11:03:41 +0200

    Version inicial V 1.0

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <title>Coches</title>
+    <meta name="description" content="Coches de gama media">
+    <link rel="stylesheet" href="css/style.css"/>
+    <style></style>
+</head>
+<body>
+    <div class="container">
+        <header id="header-home">
+            <div id="header-contacte">
+                <div id="contacte-phone">
+                    <p>CALL US TODAY 1-800-000-0000</p>
+                </div>
+                <div id="social">
+                    
+                </div>
+            </div>
+        </header>
+        <div id="container-body">
+        </div>
+    </div>    
+</body>
+</html>
