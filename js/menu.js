883ab0bde0f88f20b2878ed98587b4b09425980d
Daylin <daylinfeina@gmail.com>
2020-07-08 20:31:54 +0200

    menu RWD

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+window.onload = main();
+var cont = 1;
+
+function main(){  
+
+    document.getElementsByClassName("logo")[0].onclick = function(){   
+        
+        if(cont == 1){            
+            $('.menu').animate({
+                left:'0'
+            });
+            cont = 0;
+        }else{
+            cont = 1;
+            $('.menu').animate({
+                left:'-100%'
+            });
+        }
+
+    };
+}
